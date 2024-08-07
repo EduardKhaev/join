9c69fd127704763414058d8882e03346bad9ff78
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-08-07 08:45:08 +0200

    organized js files

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let activeUser = undefined;
 
 let activeNavItem = undefined;
 
-async function init() {
+async function initContacts() {
   await loadUsers();
   sortAllUsers();
   displayUsers();
@@ -263,88 +263,6 @@ function cancelDelete() {
   setTimeout(() => {
     document.getElementById("adduser-overlay").remove();
   }, 200);
-}
-
-function showChangeSuccess(message) {
-  let parent = document.getElementById("body");
-  let messagePopUp = elementBuilder(parent, "div", "display-message");
-  messagePopUp.innerHTML = message;
-  messagePopUp.style.opacity = "0";
-  messagePopUp.style.opacity = "1";
-  setTimeout(() => {
-    messagePopUp.style.opacity = "0";
-  }, 2000);
-  setTimeout(() => {
-    messagePopUp.remove();
-  }, 2300);
-}
-
-async function putData(path = "/names/", id, data) {
-  await fetch(FIREBASE_URL + path + id + ".json", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-}
-
-async function postData(path = "/names", data) {
-  await fetch(FIREBASE_URL + path + ".json", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-}
-
-async function deleteData(path = "/names/", id) {
-  await fetch(FIREBASE_URL + path + id + ".json", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
-
-function createOverlay(id, overlayclass = "overlay") {
-  let parent = document.getElementById("body");
-  let overlay = elementBuilder(parent, "div", overlayclass, id);
-  return overlay;
-}
-
-function elementBuilder(parent, childType, childClass, childID = "") {
-  let child = document.createElement(childType);
-  child.className = childClass;
-  child.id = childID;
-  parent.appendChild(child);
-  return child;
-}
-
-function getRandomColor() {
-  let color = "";
-  let hue = getRandomInt(255);
-  let saturation = getRandomInt(30) + 70;
-  color = hslToHex(hue, saturation, 50);
-  return color;
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function hslToHex(h, s, l) {
-  l /= 100;
-  const a = (s * Math.min(l, 1 - l)) / 100;
-  const f = (n) => {
-    const k = (n + h / 30) % 12;
-    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-    return Math.round(255 * color)
-      .toString(16)
-      .padStart(2, "0");
-  };
-  return `#${f(0)}${f(8)}${f(4)}`;
 }
 
 function setNavActive(element) {
