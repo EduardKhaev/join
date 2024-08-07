0a37ce16b16f2dcf68630b857961f91c6e64309f
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-08-07 10:45:54 +0200

    JSDoc finalfinal

--- utility.js
+++ utility.js
@@ -1,3 +1,7 @@
+/**
+ * displays feedback message after an action
+ * @param {string} message - message to display
+ */
 function showChangeSuccess(message) {
   let parent = document.getElementById("body");
   let messagePopUp = elementBuilder(parent, "div", "display-message");
@@ -12,12 +16,26 @@ function showChangeSuccess(message) {
   }, 2300);
 }
 
+/**
+ * creates an overlay modal
+ * @param {int} id - id of overlay HTMLElement
+ * @param {string} overlayclass - class of overlay HTMLElement
+ * @returns
+ */
 function createOverlay(id, overlayclass = "overlay") {
   let parent = document.getElementById("body");
   let overlay = elementBuilder(parent, "div", overlayclass, id);
   return overlay;
 }
 
+/**
+ *
+ * @param {HTMLElement} parent - parent Element to create a bew HTMLElement in
+ * @param {string} childType - HTMLElement type
+ * @param {string} childClass - class of created HTMLElement
+ * @param {string} childID - id of created HTMLElement
+ * @returns
+ */
 function elementBuilder(parent, childType, childClass, childID = "") {
   let child = document.createElement(childType);
   child.className = childClass;
@@ -26,18 +44,33 @@ function elementBuilder(parent, childType, childClass, childID = "") {
   return child;
 }
 
+/**
+ * creates a random color in hex format
+ */
 function getRandomColor() {
   let color = "";
-  let hue = getRandomInt(255);
+  let hue = getRandomInt(360);
   let saturation = getRandomInt(30) + 70;
   color = hslToHex(hue, saturation, 50);
   return color;
 }
 
+/**
+ * generates a random integer number
+ * @param {int} max
+ * @returns random int between 0 and [max]
+ */
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+/**
+ * converts a color definition in hsl to hex
+ * @param {int} h - hue (0 to 360)
+ * @param {int} s - saturation (0 to 100)
+ * @param {int} l - lightness (0 to 100)
+ * @returns color {string} in hex value
+ */
 function hslToHex(h, s, l) {
   l /= 100;
   const a = (s * Math.min(l, 1 - l)) / 100;
