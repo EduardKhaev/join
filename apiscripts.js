c3d0d4d1b2027d560aa7bf411a6bf93d2f48c95a
Galina Antipin <galina.antipin@gmail.com>
2024-08-07 10:16:54 +0200

    comments on functions

--- apiscripts.js
+++ apiscripts.js
@@ -1,3 +1,9 @@
+/**
+ * send an edit contact to database to replace the existing contact
+ * @param {string} path - the path in the database where the contact should be updated
+ * @param {string} id - the unique identifier for the contact being updated
+ * @param {object} data - the contact that will replace the existing contact 
+ */
 async function putData(path = "/names/", id, data) {
   await fetch(FIREBASE_URL + path + id + ".json", {
     method: "PUT",
@@ -8,6 +14,11 @@ async function putData(path = "/names/", id, data) {
   });
 }
 
+/**
+ * send a new contact to database
+ * @param {string} path - path in the database where the contact should be added
+ * @param {object} data - added contacts
+ */
 async function postData(path = "/names", data) {
   await fetch(FIREBASE_URL + path + ".json", {
     method: "POST",
@@ -18,6 +29,11 @@ async function postData(path = "/names", data) {
   });
 }
 
+/**
+ * delete the contact from database
+ * @param {string} path - path in the database where the contact should be deleted
+ * @param {string} id - the unique identifier for the contact to be deleted
+ */
 async function deleteData(path = "/names/", id) {
   await fetch(FIREBASE_URL + path + id + ".json", {
     method: "DELETE",
