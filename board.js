503800991f97cdc7f0830c9aba66916b68e80d20
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-08-16 15:22:27 +0200

    fixed lost function applySelectedContacts()

--- board.js
+++ board.js
@@ -239,9 +239,16 @@ function editTask(id) {
   let overlay = createOverlay("edit-task-overlay");
   overlay.innerHTML = getEditTaskContentHtml(task);
   insertContactsToInput();
+  applySelectedContacts(task);
   showEditSubtasks(task.subtasks, task.id);
   let oldOverlay = document.getElementById("task-details-overlay");
   oldOverlay.remove();
+}
+
+function applySelectedContacts(task) {
+  for (let i = 0; i < task.assigned.length; i++) {
+    markContactAssigned(task.assigned[i]);
+  }
 }
 
 /**
