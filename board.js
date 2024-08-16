ceec9b8d47c2455d219d5e09b3ea9a8e73ebcb78
s4mastig <maximilian.stigler@gmx.net>
2024-08-16 13:43:27 +0200

    nothing

--- board.js
+++ board.js
@@ -185,7 +185,7 @@ function closeTaskDetails(
   containerId = "task-large"
 ) {
   let taskDetails = document.getElementById(containerId);
-  taskDetails.classList.add("addtask-overlaycontainer-out");
+  taskDetails.classList.add("slide-out");
   setTimeout(() => {
     let overlay = document.getElementById(overlayId);
     if (overlay) {
