dc4ad6043281b4a436a49ce558b37d8c814931ea
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-08-20 20:18:56 +0200

    auth.js

---
+++ auth.js
@@ -0,0 +1,36 @@
+function checkAuthState() {
+  let token = getToken();
+  if (token == undefined) return false;
+  else if (rememberIsSet(token) || tokenIsNew(token)) {
+    passTokenData(token);
+    renewToken(token);
+    return true;
+  } else return false;
+}
+
+function getToken() {
+  let tokenString = localStorage.getItem("loginToken");
+  if (tokenString !== null) {
+    return JSON.parse(tokenString);
+  } else return;
+}
+
+function rememberIsSet(token) {
+  return token.remember === true;
+}
+
+function tokenIsNew(token) {
+  let now = Math.floor(Date.now() / 1000);
+  return now - 3600 < token.timestamp;
+}
+
+function passTokenData(token) {
+  loggedIn = {
+    name: token.name,
+    initials: token.initials,
+  };
+}
+
+function renewToken(token) {
+  setToken(token.name, token.initials, token.remember);
+}
