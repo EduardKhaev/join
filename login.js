9bf20b67165cc5a3b40c340ab61ac5791b486f04
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-08-20 09:52:27 +0200

    login html

--- login.js
+++ login.js
@@ -1,16 +1,28 @@
-function loginAsGuest() {
+let loginData = [];
+
+function loginAsGuest(event) {
+  event.preventDefault();
   console.log("Guest logged in");
 }
 
 function validateLogin(event) {
   event.preventDefault();
   let [email, password] = getLoginFormData();
+  checkLoginData(email, password);
+}
+
+function checkLoginData(username, password) {
+  console.log(username, password);
 }
 
 function getLoginFormData() {
-  let email = getLoginEmail();
-  let password = getLoginPassword();
+  let email = getLoginFieldData("entermail");
+  let password = getLoginFieldData("enterpassword");
   return [email, password];
+}
+
+function getLoginFieldData(field) {
+  return document.getElementById(field).value;
 }
 
 function showInvalidLogin(labelId, inputId) {
@@ -21,4 +33,12 @@ function showInvalidLogin(labelId, inputId) {
 function removeInvalidLogin(labelId, inputId) {
   document.getElementById(inputId).style = "";
   document.getElementById(labelId).style = "opacity: 0;";
+}
+
+function unknownUser() {
+  console.log("Username not registered");
+}
+
+function incorrectPassword() {
+  console.log("Password not correct");
 }
